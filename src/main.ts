de214913f058e14bdd6aa293d74e3991d058161a
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-10-08 14:47:32 -0700

    Change the order of validations

--- src/main.ts
+++ src/main.ts
@@ -5,33 +5,13 @@ import { ApplyPerspectiveEffectHandler } from "./types"
 
 export default function () {
   once<ApplyPerspectiveEffectHandler>("APPLY_PERSPECTIVE_EFFECT", async function (code: string) {
-    const target = figma.currentPage.selection[0]
-
-    console.log("[main] target type:", target.type)
-
-    if (!target || target.type !== "VECTOR") {
-      figma.notify(
-        "Please select a vector node by creating a shape and using the `edit object` tool"
-      )
+    if (!checkValidSelection()) {
       return
     }
 
-    if (
-      isArray(target.fills) === false ||
-      typeof target.fills === "symbol" ||
-      target.fills.length === 0
-    ) {
-      figma.notify("Please make sure the selected shape has a fill")
-      return
-    }
+    const target = figma.currentPage.selection[0] as VectorNode
 
-    const fill = target.fills[0]
-
-    if (fill.type !== "IMAGE") {
-      figma.notify("Please make sure the selected shape has an image fill")
-      return
-    }
-
+    const fill = (target.fills as [ImagePaint])[0]
     const image = fill.imageHash
     fill.imageTransform
 
@@ -39,5 +19,43 @@ export default function () {
     figma.viewport.scrollAndZoomIntoView([target])
     figma.closePlugin()
   })
+
+  if (!checkValidSelection()) {
+    figma.closePlugin()
+    return
+  }
+
   showUI({ height: 240, width: 320 })
+}
+
+function checkValidSelection() {
+  const target = figma.currentPage.selection[0]
+
+  if (!target || target.type !== "VECTOR") {
+    figma.notify("Please select a vector node by creating a shape and using the `edit object` tool")
+    return false
+  }
+
+  if (
+    isArray(target.fills) === false ||
+    typeof target.fills === "symbol" ||
+    target.fills.length === 0
+  ) {
+    figma.notify("Please make sure the selected shape has a fill")
+    return false
+  }
+
+  const fill = target.fills[0]
+
+  if (fill.type !== "IMAGE") {
+    figma.notify("Please make sure the selected shape has an image fill")
+    return false
+  }
+
+  if (target.vectorNetwork.vertices.length !== 4) {
+    figma.notify("Please make sure the selected shape has 4 vertices")
+    return false
+  }
+
+  return true
 }
